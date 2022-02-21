47cfb86810411aea57ec6f131215bc5fdf7dbee4
davi75a <98020640+davi75a@users.noreply.github.com>
2022-02-21 12:24:59 +0100

    rw

---
+++ EXAMENENTORNOS/src/move/EJERCICIO6.java
@@ -0,0 +1,22 @@
+package move;
+
+/** @author AdrianGarcia 
+@version 1.0
+@param b
+@since 5/4/2021
+@see mensaje */
+
+
+public class EJERCICIO6 implements e6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hola mundo");
+		
+		
+		
+
+	}
+
+}
